e2de1deb31af4f390157c3b8891d7fef38f49014
Gilles <gilles.hoarau@edu.ece.fr>
2017-02-17 12:14:08 +0100

    Maps
    Configuration of the map page and the nav bar

--- app/src/main/java/ppe/ece/fr/newwheels/MainPage.java
+++ app/src/main/java/ppe/ece/fr/newwheels/MainPage.java
@@ -84,6 +84,8 @@ public class MainPage extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
+        } else if (id == R.id.nav_view1) {
+
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
